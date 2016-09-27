fdc8a41167bf5a01a4cb7311235b02f390031073
wengang <tao-yu@hotmail.com>
2016-09-27 11:47:41 +0800

    use vm.$mount('#app) method or use vue component build the app ,the table display incorrectly

--- main.js
+++ main.js
@@ -1,3 +1,25 @@
 new Vue({
-  el: 'app'
-})
+  template: `
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+                <table class="table">
+                    <tr><td>#</td>
+                    <td>First Name</td>
+                    <td>Last Name</td></tr>
+                    <tr>
+                        <td>1</td>
+                        <td>Mark</td>
+                        <td>Otto</td>
+                    </tr>
+                    <tr>
+                        <td>2</td>
+                        <td>Jacob</td>
+                        <td>Tomash</td>
+                    </tr>
+                </table>
+            </div>
+        </div>
+    </div>
+  `
+}).$mount('#app')
